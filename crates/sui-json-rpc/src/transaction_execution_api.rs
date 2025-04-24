0c6676bdfaf1168e30fe7a9524eca7210f4fbc8b
flinny <flinny.us@gmail.com>
2025-04-24 17:17:06 +0000

    add multi object ignore

--- crates/sui-json-rpc/src/transaction_execution_api.rs
+++ crates/sui-json-rpc/src/transaction_execution_api.rs
@@ -307,7 +307,7 @@ impl TransactionExecutionApi {
             &object_cache,
             &transaction_effects,
             input_objs,
-            mock_gas,
+            mock_gas.map(|obj| vec![obj]),
         )
         .await?;
         let object_changes = get_object_changes(
@@ -347,7 +347,7 @@ impl TransactionExecutionApi {
             &object_cache,
             &transaction_effects,
             input_objs,
-            mock_gas,
+            mock_gas.map(|obj| vec![obj]),
         )
         .await?;
         let object_changes = get_object_changes(

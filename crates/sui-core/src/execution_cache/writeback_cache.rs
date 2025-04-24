bf325fd2866427a3b9980366d3d5628b8942ead7
flinny <flinny.us@gmail.com>
2025-04-24 17:50:15 +0000

    add method

--- crates/sui-core/src/execution_cache/writeback_cache.rs
+++ crates/sui-core/src/execution_cache/writeback_cache.rs
@@ -2078,6 +2078,10 @@ impl TransactionCacheRead for WritebackCache {
 }
 
 impl ExecutionCacheWrite for WritebackCache {
+    fn reload_objects(&self, objects: Vec<ObjectID>) {
+        self.reload_cached(&objects);
+    }
+    
     fn acquire_transaction_locks(
         &self,
         epoch_store: &AuthorityPerEpochStore,

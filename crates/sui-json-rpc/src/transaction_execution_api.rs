fcdcb86da0dfa04c82293758dea0a5cf498098fe
flinny <flinny.us@gmail.com>
2025-04-24 14:10:41 +0000

    add cache for final diff

--- crates/sui-json-rpc/src/transaction_execution_api.rs
+++ crates/sui-json-rpc/src/transaction_execution_api.rs
@@ -297,9 +297,12 @@ impl TransactionExecutionApi {
         let sender = txn_data.sender();
         let (resp, written_objects, transaction_effects, mock_gas) = self
             .state
-            .dry_exec_transaction_override_objects_trait(txn_data.clone(), txn_digest, override_objects)
+            .dry_exec_transaction_override_objects_trait(txn_data.clone(), txn_digest, override_objects.clone())
             .await?;
-        let object_cache = ObjectProviderCache::new_with_cache(self.state.clone(), written_objects);
+        let mut object_cache = ObjectProviderCache::new_with_cache(self.state.clone(), written_objects);
+        object_cache.insert_objects_into_cache(
+            override_objects.into_iter().map(|(_id, obj)| obj).collect(),
+        );
         let balance_changes = get_balance_changes_from_effect(
             &object_cache,
             &transaction_effects,

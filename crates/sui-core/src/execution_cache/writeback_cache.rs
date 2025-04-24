f664cb73b9d5c76d903bdace9f33fcc9a430b9de
flinny <flinny.us@gmail.com>
2025-04-24 18:04:20 +0000

    add catchup

--- crates/sui-core/src/execution_cache/writeback_cache.rs
+++ crates/sui-core/src/execution_cache/writeback_cache.rs
@@ -2080,6 +2080,15 @@ impl TransactionCacheRead for WritebackCache {
 }
 
 impl ExecutionCacheWrite for WritebackCache {
+    fn update_underlying(&self) {
+        use typed_store::Map;
+        self.store
+            .perpetual_tables
+            .objects
+            .try_catch_up_with_primary()
+            .unwrap();
+    }
+    
     fn reload_objects(&self, objects: Vec<(ObjectID, Object)>) {
         self.reload_cached(objects);
     }

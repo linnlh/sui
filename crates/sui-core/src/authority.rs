8b92dc21d1ee12f38c8986ccd9286598a8aeac6a
flinny <flinny.us@gmail.com>
2025-04-25 05:15:10 +0000

    communiate less

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -1562,7 +1562,7 @@ impl AuthorityState {
         epoch_store: &Arc<AuthorityPerEpochStore>,
     ) -> SuiResult {
         let raw_events = inner_temporary_store.events.clone();
-        let sui_events = raw_events
+        let sui_events: Vec<SuiEvent> = raw_events
             .data
             .iter()
             .enumerate()
@@ -1625,14 +1625,19 @@ impl AuthorityState {
 
         self.get_cache_writer()
             .write_transaction_outputs(epoch_store.epoch(), Arc::clone(&transaction_outputs));
-        // self.cache_update_handler
-        //     .update_cache(package_updates)
-        //     .await;
-        let changed_objects = transaction_outputs
-            .written
-            .keys()
-            .copied()
-            .collect::<Vec<_>>();
+
+        
+        if !certificate.transaction_data().is_system_tx() {
+            let changed_objects = transaction_outputs
+                .written
+                .keys()
+                .copied()
+                .collect::<Vec<_>>();
+            // if !changed_objects.is_empty() && !sui_events.is_empty() {
+            //     self.cache_update_handler
+            //         .notify_reload_objects(changed_objects);
+            // }
+        }
 
         if certificate.transaction_data().is_end_of_epoch_tx() {
             // At the end of epoch, since system packages may have been upgraded, force
@@ -1644,9 +1649,15 @@ impl AuthorityState {
         // commit_certificate finished, the tx is fully committed to the store.
         tx_guard.commit_tx();
 
-        let _ = self
+        if !certificate.transaction_data().is_system_tx()
+            && !sui_events.is_empty()
+            && !transaction_outputs.written.is_empty()
+        {
+            let _ = self
             .tx_handler
             .send_tx_effects_and_events(effects, sui_events);
+        }
+        
 
         // Notifies transaction manager about transaction and output objects committed.
         // This provides necessary information to transaction manager to start executing
